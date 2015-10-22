0062f448e8090096b1dd55932d222e673e8ae0bf
Ulf Wiger <ulf@feuerlabs.com>
2015-10-22 14:34:44 +0200

    function_clause in delete_globals

--- src/gproc_dist.erl
+++ src/gproc_dist.erl
@@ -836,6 +836,8 @@ delete_globals(Globals) ->
     lists:foreach(
       fun({{_,g,_},T} = K) when is_atom(T); is_pid(T) ->
               ets:delete(?TAB, K);
+	 ({{{_,g,_},T} = K, _}) when is_atom(T); is_pid(T) ->
+	      ets:delete(?TAB, K);
          ({Pid, Key}) when is_pid(Pid); Pid==shared ->
 	      ets:delete(?TAB, {Pid, Key})
       end, Globals).
